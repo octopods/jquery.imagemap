1087d4e7dc923dbb083edcb5dc9e550b36b4ca99
aluminiumgeek <mihail265@gmail.com>
2014-07-27 15:52:28 +0400

    last point equals first one

--- jquery.imagemap.js
+++ jquery.imagemap.js
@@ -53,29 +53,55 @@ imagemap = (function($) {
   }
   
   function get_area(coords) {
-    var area = $('<area />').attr({
-      shape: 'poly',
-      href: '#'
-    });
     
-    coords.map(function(coord) {
-      var coords_attr = area.attr('coords');
-      
-      area.attr(
-        'coords',
-        '{0}{1},{2}'.format(
-          coords_attr ? coords_attr+',' : '', 
-          coord.x, 
-          coord.y
-        )
-      );
-    });
+    if (coords) {
+      var area = $('<area />').attr({
+        shape: 'poly',
+        href: '#'
+      });
+    
+      coords.map(function(coord, index) {
+        var coords_attr = area.attr('coords');
+        
+        if (index != coords.length-1) {
+          coords_attr = '{0}{1},{2}'.format(
+            coords_attr ? coords_attr+',' : '', 
+            coord.x, 
+            coord.y
+          )
+        }
+        else {
+          // last point must equal the first one
+          
+          coords_attr = '{0},{1},{2}'.format(
+            area.attr('coords'),
+            coords[0].x,
+            coords[0].y
+          )
+        }
+        
+        area.attr('coords', coords_attr);
+      });
+    }
     
     return area;
   }
   
   function get_name() {
     return 'm{0}'.format((new Date).getTime());
+  }
+  
+  if (!String.prototype.format) {
+    String.prototype.format = function() {
+      var args = arguments;
+    
+      return this.replace(/{(\d+)}/g, function(match, number) { 
+        return typeof args[number] != 'undefined'
+          ? args[number]
+          : match
+        ;
+      });
+    };
   }
   
   return self;
